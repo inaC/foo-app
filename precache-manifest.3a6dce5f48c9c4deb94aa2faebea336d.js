277a1c35a29104731dad56364b6ebdc17bbde459
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-03-11 12:45:20 -0300

    Updates

---
+++ precache-manifest.3a6dce5f48c9c4deb94aa2faebea336d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd6be8449926e266adf76f60df70701",
+    "url": "/foo-app/index.html"
+  },
+  {
+    "revision": "d2bc3ddfdb25c2b08c4f",
+    "url": "/foo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61138d1766d3a32ef357",
+    "url": "/foo-app/static/js/2.23166292.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/foo-app/static/js/2.23166292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bc3ddfdb25c2b08c4f",
+    "url": "/foo-app/static/js/main.73c786fd.chunk.js"
+  },
+  {
+    "revision": "130f6be84adbcce9b266",
+    "url": "/foo-app/static/js/runtime-main.1455cc2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
